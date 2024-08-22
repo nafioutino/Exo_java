e9ffd31c744792dc39004574b8ff619c5e48bd55
Abdoul-Nafiou <nafiou531@gmail.com>
2024-08-22 11:33:30 +0100

    methodes ajouté

--- Projet_gestion_employé/App.java
+++ Projet_gestion_employé/App.java
@@ -1,35 +1,112 @@
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class App {
 
+    List<Employes> employesList = new ArrayList<Employes>(15);
+
+    // methode pour ajouter un employé
+    public void addEmploye(Employes employe) {
+        employesList.add(employe);
+    }
+
+    // methode pour afficher tous les employés
+    public void showAllEmployees() {
+        for (Employes employee : employesList) {
+            System.out.println(employee);
+        }
+    }
+
+    // methode pour supprimer un employé
+    public void removeEmploye(int id) {
+        employesList.removeIf(e -> e.getId() == id);
+        System.out.println(" Employé supprimé avec succès ! ");
+    }
+
+    // methode pour afficher un employé par son id
+    public void showEmployeById(int id) {
+        for (Employes employe : employesList) {
+            if (employe.getId() == id) {
+                System.out.println(employe);
+                return;
+            }
+        }
+        System.out.println("Employé avec ID " + id + " non trouvé.");
+    }
+
     public static void main(String[] args) {
-        ArrayList<Employes> employesList = new ArrayList<Employes>(5);
-        int ID = 0;
+        App gestion = new App();
         Scanner sc = new Scanner(System.in);
-        System.out.println("Entrez le nom de l'employé: ");
-        String employeName = sc.nextLine();
 
-        System.out.println("Entrez le poste de l'employé: ");
-        String employePost = sc.nextLine();
+        System.out.println("---------------Bienvenue sur notre plateforme de gestion d'employés ! ---------------");
 
-        System.out.println("Entrez le salaire de l'employé: ");
-        int employeSal = sc.nextInt();
-        sc.close();
+        System.out.println(" Veuillez choisir une option: ");
+        while (true) {
+            System.out.println("Que voulez-vous faire sur un employé ?");
+            System.out.println("1. Ajouter un employé");
+            System.out.println("2. Supprimer un employé par ID");
+            System.out.println("3. Afficher tous les employés");
+            System.out.println("4. Afficher un employé par ID");
+            System.out.println("5. Quitter");
+            System.out.print("Choix : ");
+            int choix = sc.nextInt();
 
-        Employes employee = new Employes(ID++, employeName, employePost, employeSal);
-        employesList.add(employee);
+            switch (choix) {
+                case 1:
+                    System.out.println("Combien d'employés voulez vous ajouter ?: ");
+                    int employeNum = sc.nextInt();
+                    int i = 1;
+                    while (i <= employeNum) {
 
-        for (Employes em : employesList) {
-            System.out.println(em);
-            
+                        System.out.println("Entrez l'ID de l'employé " + i + ": ");
+                        int employeID = sc.nextInt();
+                        sc.nextLine();
+
+                        System.out.println("Entrez le nom de l'employé " + i + ": ");
+                        String employeName = sc.nextLine();
+
+                        System.out.println("Entrez le poste de l'employé " + i + ": ");
+                        String employePost = sc.nextLine();
+
+                        System.out.println("Entrez le salaire de l'employé " + i + ": ");
+                        double employeSalaire = sc.nextDouble();
+                        System.out.println("Employé "+ i + " ajouté !");
+
+                        Employes employee = new Employes(employeID, employeName, employePost, employeSalaire);
+                        gestion.addEmploye(employee);
+                        i++;
+                    }
+                    break;
+
+                case 2:
+                    System.out.println("Entrer l'ID de l'employé à supprimer: ");
+                    int idAsupprimer = sc.nextInt();
+                    gestion.removeEmploye(idAsupprimer);
+                    break;
+                case 3:
+                    System.out.println("------------Liste des employés-------------");
+                    gestion.showAllEmployees();
+                    break;
+                case 4:
+                    System.out.println("Entrer l'ID de l'employé à afficher: ");
+                    int idAafficher = sc.nextInt();
+                    gestion.showEmployeById(idAafficher);
+                    break;
+                case 5:
+                    System.out.println("Au revoir , Merci pour votre fidélité !");
+                    sc.close();
+                    System.exit(0);
+                    break;
+
+                default:
+                    System.out.println("Choix invalid !");
+                    break;
+            }
+
         }
 
-        System.out.println( "ID:" + ID);
-        System.out.println( "Name: " + employee.name);
-        System.out.println( "Poste: " + employee.post);
-        System.out.println( "Salaire: " + employee.salaire);
-        
     }
 
 }
--- Projet_gestion_employé/Employes.java
+++ Projet_gestion_employé/Employes.java
@@ -4,10 +4,10 @@ public class Employes {
     int ID;
     String name;
     String post;
-    int salaire;
+    double salaire;
      
 
-    public Employes (int ID, String name, String post, int salaire){
+    public Employes (int ID, String name, String post, double salaire){
         this.ID = ID;
         this.name = name;
         this.post = post;
@@ -50,12 +50,16 @@ public class Employes {
 
 
     // get et set pour ID
-    public int getSalaire(){
+    public double getSalaire(){
         return this.salaire;
     }
 
-    public void setSalaire(int salaire){
+    public void setSalaire(double salaire){
         this.salaire = salaire;
+    }
+
+    public String toString() {
+        return "Employe [ID = " + ID + ", Nom= " + name + ", Poste= " + post + ", Salaire= " + salaire + "]";
     }
 
 
